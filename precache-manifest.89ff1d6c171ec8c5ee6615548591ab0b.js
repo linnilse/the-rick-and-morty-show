1e7c33ec11a8e5863d63475fe2709a2e799b6f3b
Linda Nilsson <linda@linnil.se>
2019-03-28 15:37:54 +0100

    Updates

---
+++ precache-manifest.89ff1d6c171ec8c5ee6615548591ab0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "54dc187b87817217f03a",
+    "url": "/the-rick-and-morty-show/static/js/runtime~main.a68bcbc0.js"
+  },
+  {
+    "revision": "4763e11e28aa14328f11",
+    "url": "/the-rick-and-morty-show/static/js/main.5086bb30.chunk.js"
+  },
+  {
+    "revision": "ab72976e5bc969eb2e7e",
+    "url": "/the-rick-and-morty-show/static/js/2.b013b3f6.chunk.js"
+  },
+  {
+    "revision": "4763e11e28aa14328f11",
+    "url": "/the-rick-and-morty-show/static/css/main.082541cb.chunk.css"
+  },
+  {
+    "revision": "2fa796f080793215e8c39fd9d9f4ee6e",
+    "url": "/the-rick-and-morty-show/index.html"
+  }
+];
